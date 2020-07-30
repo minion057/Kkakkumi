10709db57de10a014642ff55b48a51ef8db7bec4
cjswo7119 <42957826+cjswo7119@users.noreply.github.com>
2020-07-30 20:52:33 +0900

    모션감지 액티비티 밑바탕 구성

--- Kkakkumi/app/src/main/java/Khack/Q/Kkakkumi/MainActivity.java
+++ Kkakkumi/app/src/main/java/Khack/Q/Kkakkumi/MainActivity.java
@@ -2,7 +2,10 @@ package Khack.Q.Kkakkumi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     //<editor-fold desc="변수 선언">
@@ -24,6 +27,21 @@ public class MainActivity extends AppCompatActivity {
         //뒤로가기(back key) 제어(종료)관리할 Activity가 현재 Activity라고 값을 입력
         backPressHandler = new BackPressHandler(this);
         //</editor-fold>
+        //</editor-fold>
+
+        //<editor-fold desc="교육 진행 화면 이동">
+        // 교육 시작 화면으로 넘어가기 위한 클릭리스너
+        Button btnEduStart = (Button) findViewById(R.id.btnStart);
+        /**
+         * 교육 선택에 따른 화면 activity 선택기능 구현 필요
+         */
+        btnEduStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WashHandActivity.class);
+                startActivity(intent);
+            }
+        });
         //</editor-fold>
     }
 
---
+++ Kkakkumi/app/src/main/java/Khack/Q/Kkakkumi/WashHandActivity.java
@@ -0,0 +1,14 @@
+package Khack.Q.Kkakkumi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WashHandActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wash_hand);
+    }
+}
